cea72c52119630e24a14be669fc09ac21ccbceac
Duc Tran <tblyduc2412@gmail.com>
2023-03-08 17:15:32 +0700

    refactor

--- src/cloudinary/uploadImage.ts
+++ src/cloudinary/uploadImage.ts
@@ -1,8 +1,4 @@
 export async function uploadImage(file: any) {
-    //     // Lấy file ảnh từ input tag
-    // const fileInput = document.getElementById("file-input");
-    // const file = fileInput.files[0];
-
     // Cài đặt các thông số cho việc upload
     const cloudName = 'leetb';
     const url = `https://api.cloudinary.com/v1_1/${cloudName}/upload`;
@@ -13,12 +9,10 @@ export async function uploadImage(file: any) {
     // Tạo đối tượng FormData để chứa ảnh
     const formData = new FormData();
     formData.append('file', file);
-    formData.append('upload_preset', uploadPreset);
     formData.append('public_id', file.name);
-    // formData.append('api_key', apiKey);
+    formData.append('upload_preset', uploadPreset);
 
     // Thực hiện upload bằng Fetch API
-
     try {
         const res = await fetch(url, {
             method: 'POST',
