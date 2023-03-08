bf51311eb55a157bebc9b61ca0f70525da6d24a4
Duc Tran <tblyduc2412@gmail.com>
2023-03-08 17:04:51 +0700

    refactor

--- src/constants/index.ts
+++
@@ -1,1 +0,0 @@
-export const MOCK_URL = `https://62c4071b7d83a75e39edba50.mockapi.io/`;
--- src/main.ts
+++ src/main.ts
@@ -4,29 +4,5 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import './style.css';
 import './utils/bootstrapFormValidation';
 
-import './features/AddMenuItem';
-// import { data } from './data';
-
-// const menu = <HTMLDivElement>document.querySelector('#menu');
-// const menuTable = <HTMLTableElement>menu.querySelector('table');
-// menuTable.tBodies[0].innerHTML = data
-//     .map((item, index) => {
-//         return /*html*/ `
-//     <tr>
-//         <th scope="row">${index + 1}</th>
-//         <td>${item.getId()}</td>
-//         <td>
-//             <img src="${item.getImage()}" alt="" style="width: 100px">
-//         </td>
-//         <td>${item.getName()}</td>
-//         <td>${item.getPrice()}</td>
-//         <td></td>
-//         <td>${item.getCreateAt().toLocaleDateString()}</td>
-//         <td>
-//             <button class="btn btn-warning"><i class="bi bi-pencil"></i></button>
-//             <button class="btn btn-danger"><i class="bi bi-trash"></i></button>
-//         </td>
-//     </tr>
-//     `;
-//     })
-//     .join('');
+import './features/Menu/AddMenuItem';
+import './features/Menu/showMenu';
