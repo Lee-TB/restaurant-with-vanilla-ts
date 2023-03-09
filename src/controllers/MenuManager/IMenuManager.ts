00a5edf65eb69c22ff1b5242b19e910d3e5b1faa
Lee TB <tblyduc2412@gmail.com>
2023-03-10 02:15:22 +0700

    remove: controllers folder

--- src/controllers/MenuManager/IMenuManager.ts
+++
@@ -1,8 +0,0 @@
-import { MenuType } from '../../models/enums/MenuType';
-
-export interface IMenuManager {
-    createMenuItem(): void;
-    updateMenuItem(id: number): void;
-    deleteMenuItem(id: number): void;
-    renderMenu(type: MenuType): Promise<void>;
-}
