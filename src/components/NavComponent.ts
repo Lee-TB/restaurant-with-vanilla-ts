24a239218026c5f5f4bf1de20ac0f181661c4fb5
Lee TB <tblyduc2412@gmail.com>
2023-03-11 20:00:32 +0700

    feat: NavComponent with onChange behavior.

--- src/components/NavComponent.ts
+++ src/components/NavComponent.ts
@@ -4,22 +4,21 @@ import { BaseComponent } from './BaseComponent';
 export interface NavItemProps {
     key: string;
     label: string;
-    children?: string;
 }
 
 export interface NavComponentProps {
-    key: string;
-    type: 'tab';
+    type?: 'tab';
     items: NavItemProps[];
-    onChange: (activeKey: string) => void;
-    activeKey: string;
+    activeKey?: string;
+    onChange?: (activeKey: string) => void;
 }
 
 export class NavComponent extends BaseComponent {
     private key: string;
-    private type?: 'tab';
     private items: NavItemProps[];
+    private type?: 'tab';
     private activeKey?: string;
+    private onChange?: (activeKey: string) => void;
 
     constructor(element: HTMLElement, props?: NavComponentProps) {
         super(element);
@@ -27,6 +26,7 @@ export class NavComponent extends BaseComponent {
         this.type = props?.type;
         this.items = props?.items || [];
         this.activeKey = props?.activeKey || props?.items[0].key;
+        this.onChange = props?.onChange;
     }
 
     render(): void {
@@ -34,25 +34,53 @@ export class NavComponent extends BaseComponent {
             <ul class="nav${this.type === 'tab' ? ' nav-tabs' : ''}" id="${
             this.key
         }">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#"></a>
-                </li>
             </ul>
         `;
+
+        this.renderItems();
+
+        this.listenClick();
     }
 
-    renderItems() {
+    private renderItems() {
         const navElement = <HTMLUListElement>document.getElementById(this.key);
         navElement.innerHTML = this.items
             .map((item) => {
                 return /* html */ `
-                <li class="nav-item" key="${item.key}">
-                    <a class="nav-link" href="#">
+                <li class="nav-item ${this.key}-nav-item">
+                    <a 
+                        id="${item.key}"
+                        class="nav-link ${this.key}-nav-link ${
+                    this.activeKey === item.key ? 'active' : ''
+                }" 
+                        href="#"
+                    >
                         ${item.label}
                     </a>
                 </li>
             `;
             })
             .join('');
+    }
+
+    private listenClick() {
+        const navLinkElementList = <NodeListOf<HTMLLinkElement>>(
+            document.querySelectorAll(`.${this.key}-nav-link`)
+        );
+
+        navLinkElementList.forEach((navLink) => {
+            navLink.addEventListener('click', () => {
+                /* css active link when click. First remove active if it exist, then add active to the link what was clicked */
+                navLinkElementList.forEach((navLinkStyle) => {
+                    navLinkStyle.classList.remove('active');
+                });
+                navLink.classList.add('active');
+
+                /* pass active key to onChange callback */
+                if (this.onChange) {
+                    this.onChange(navLink.id);
+                }
+            });
+        });
     }
 }
