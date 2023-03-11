02df676075d7c45c508f5bfd9554c577e4e6240a
Lee TB <tblyduc2412@gmail.com>
2023-03-11 11:54:42 +0700

    feat: add get, update, delete methods to MenuAPI class

--- src/api/MenuAPI.ts
+++ src/api/MenuAPI.ts
@@ -3,31 +3,88 @@ export const MOCK_URL = `https://62c4071b7d83a75e39edba50.mockapi.io/`;
 export class MenuAPI {
     private url: string;
     constructor(endpoint: string) {
-        this.url = MOCK_URL + endpoint;
+        this.url = MOCK_URL + endpoint + '/';
     }
 
     async post(data: any) {
-        const res = await fetch(this.url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-        });
-        return new Promise((resolve) => {
-            resolve(res);
+        return new Promise(async (resolve, reject) => {
+            try {
+                const res = await fetch(this.url, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(data),
+                });
+                resolve(res);
+            } catch (error) {
+                reject(error);
+            }
         });
     }
 
     async getAll() {
-        const res = await fetch(this.url, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
+        return new Promise(async (resolve, reject) => {
+            try {
+                const res = await fetch(this.url, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                });
+                resolve(res);
+            } catch (error) {
+                reject(error);
+            }
         });
-        return new Promise((resolve) => {
-            resolve(res);
+    }
+
+    async get(id: number) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const res = await fetch(this.url + id, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                });
+                resolve(res);
+            } catch (error) {
+                reject(error);
+            }
+        });
+    }
+
+    async update(id: number, data: any) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const res = await fetch(this.url + id, {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(data),
+                });
+                resolve(res);
+            } catch (error) {
+                reject(error);
+            }
+        });
+    }
+
+    async delete(id: number) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const res = await fetch(this.url + id, {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                });
+                resolve(res);
+            } catch (error) {
+                reject(error);
+            }
         });
     }
 }
