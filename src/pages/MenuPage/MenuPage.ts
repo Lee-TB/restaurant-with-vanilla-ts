8da323e35643c72305e8edcf5827bf252a0d1a4f
Lee TB <tblyduc2412@gmail.com>
2023-03-11 12:00:27 +0700

    refactor: rename some variables and html id attribute.

--- src/pages/MenuPage/MenuPage.ts
+++ src/pages/MenuPage/MenuPage.ts
@@ -13,36 +13,39 @@ export class MenuPage extends BaseComponent {
         const html = /*html */ `
             <section id="menu">
                 <h2>Menu Management</h2>
-                <div id="menuTabsElement"></div>
+                <div id="menuTabsPlaceholder"></div>
                 <div class="d-flex justify-content-end p-2">
-                <div id="openAddMenuItemModalButtonElement"></div>                    
+                <div id="openAddMenuItemModalButtonPlaceholder"></div>                    
                 </div>
-                <div id="menuTableElement"></div>                
+                <div id="menuTablePlaceholder"></div>                
             </section>
         `;
 
         this.element.innerHTML = html;
 
         /* Render Menu Tabs */
-        const menuTabsElement = <HTMLDivElement>(
-            document.getElementById('menuTabsElement')
+        const menuTabsPlaceholder = <HTMLDivElement>(
+            document.getElementById('menuTabsPlaceholder')
         );
-        new MenuTabs(menuTabsElement).render();
+        const menuTabs = new MenuTabs(menuTabsPlaceholder);
+        menuTabs.render();
 
         /* Render Add Menu Item Modal Button */
-        const openAddMenuItemModalButtonElement = <HTMLDivElement>(
-            document.getElementById('openAddMenuItemModalButtonElement')
+        const openAddMenuItemModalButtonPlaceholder = <HTMLDivElement>(
+            document.getElementById('openAddMenuItemModalButtonPlaceholder')
         );
-        new AddMenuItemModalButton(openAddMenuItemModalButtonElement).render();
+        new AddMenuItemModalButton(
+            openAddMenuItemModalButtonPlaceholder
+        ).render();
 
         /* Render Menu Table */
-        const menuTableElement = <HTMLDivElement>(
-            document.getElementById('menuTableElement')
+        const menuTablePlaceholder = <HTMLDivElement>(
+            document.getElementById('menuTablePlaceholder')
         );
-        const menuTable = new MenuTable(menuTableElement);
+        const menuTable = new MenuTable(menuTablePlaceholder);
         menuTable.render();
 
-        /* Switch Menu Type */
+        /* Render when Switch Menu Type */
         const menuTabElements = <NodeListOf<HTMLLinkElement>>(
             document.querySelectorAll('.menuTabs')
         );
