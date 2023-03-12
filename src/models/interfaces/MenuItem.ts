275e3e9a17d12e13bf2dafe54b6c563583a9b2d0
Lee TB <tblyduc2412@gmail.com>
2023-03-12 10:17:50 +0700

    feat: craete MenuItem type by interface

---
+++ src/models/interfaces/MenuItem.ts
@@ -0,0 +1,10 @@
+export interface MenuItem {
+    id: string;
+    name: string;
+    price: number;
+    image: string;
+    description: string;
+    menuType: string;
+    categories: string;
+    createAt: Date;
+}
