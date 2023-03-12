04cd36fc0ebcb7e99a1d9aaa27230182a50ef4af
Lee TB <tblyduc2412@gmail.com>
2023-03-12 13:35:19 +0700

    feat: add setBody, setTitle, setFooter callbacks to receive elements outside the component.

--- src/components/ModalComponent.ts
+++ src/components/ModalComponent.ts
@@ -2,22 +2,26 @@ import { Modal } from 'bootstrap';
 import { BaseComponent } from '../components/BaseComponent';
 
 export interface ModalProps {
-    title: string;
-    body: string;
-    footer?: string;
+    setBody?: (element: HTMLElement) => void;
+    setTitle?: (element: HTMLElement) => void;
+    setFooter?: (element: HTMLElement) => void;
 }
 
 export class ModalComponent extends BaseComponent {
     private static modalElement: HTMLElement;
-    private title: string;
-    private body: string;
-    private footer?: string;
+    private titleElement?: HTMLElement;
+    private bodyElement?: HTMLElement;
+    private footerElement?: HTMLElement;
+    private setBody?: (element: HTMLElement) => void;
+    private setTitle?: (element: HTMLElement) => void;
+    private setFooter?: (element: HTMLElement) => void;
 
     constructor(element: HTMLElement, props: ModalProps) {
         super(element);
-        this.title = props.title;
-        this.body = props.body;
-        this.footer = props.footer || '';
+
+        this.setTitle = props.setTitle;
+        this.setBody = props.setBody;
+        this.setFooter = props.setFooter;
     }
 
     render(): void {
@@ -36,7 +40,7 @@ export class ModalComponent extends BaseComponent {
                                 class="modal-title fs-5"
                                 id="ModalTitle"
                             >
-                                ${this.title}
+                                
                             </h1>
                             <button
                                 type="button"
@@ -45,14 +49,9 @@ export class ModalComponent extends BaseComponent {
                                 aria-label="Close"
                             ></button>
                         </div>
-                        <div class="modal-body">                            
-                            ${this.body}                            
+                        <div class="modal-body" id='ModalBody'>                                                                                 
                         </div>
-                        ${
-                            this.footer
-                                ? `<div class="modal-footer">${this.footer}</div>`
-                                : ''
-                        }                        
+                        <div class="modal-footer" id="ModalFooter"></div>                    
                     </div>
                 </div>
             </div>
@@ -63,6 +62,48 @@ export class ModalComponent extends BaseComponent {
         ModalComponent.modalElement = <HTMLDivElement>(
             document.getElementById('modalElement')
         );
+
+        this.initiatePropsElement();
+        this.renderComponents();
+    }
+
+    /**Initialize DOM elements */
+    private initiatePropsElement() {
+        this.titleElement = <HTMLElement>document.getElementById('ModalTitle');
+        this.bodyElement = <HTMLElement>document.getElementById('ModalBody');
+        this.footerElement = <HTMLElement>(
+            document.getElementById('ModalFooter')
+        );
+    }
+
+    /**Pass DOM elements into callbacks to render */
+    private renderComponents() {
+        if (this.setTitle && this.titleElement) {
+            this.setTitle(this.titleElement);
+        }
+
+        if (this.setBody && this.bodyElement) {
+            this.setBody(this.bodyElement);
+        }
+
+        if (this.setFooter && this.footerElement) {
+            this.setFooter(this.footerElement);
+        }
+    }
+
+    /**Change the content of the modal after it has been constructed */
+    public setProps(props: ModalProps) {
+        if (this.titleElement && props.setTitle) {
+            props.setTitle(this.titleElement);
+        }
+
+        if (this.bodyElement && props.setBody) {
+            props.setBody(this.bodyElement);
+        }
+
+        if (this.footerElement && props.setFooter) {
+            props.setFooter(this.footerElement);
+        }
     }
 
     /**
