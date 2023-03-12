449fa0d7d2c9fceaabdbc49acfc18298f75ad928
Lee TB <tblyduc2412@gmail.com>
2023-03-13 04:53:51 +0700

    feat: COMPLETE Menu Page

--- src/pages/MenuPage/MenuPage.ts
+++ src/pages/MenuPage/MenuPage.ts
@@ -1,7 +1,10 @@
 import { BaseComponent } from '../../components/BaseComponent';
-import { MenuTable } from './screens/MenuTable';
-import { MenuTabs } from './screens/MenuTabs';
-import { AddMenuItemModalButton } from './screens/AddMenuItemModalButton';
+import { MenuTableScreen } from './screens/MenuTableScreen';
+import { NavComponent } from '../../components/NavComponent';
+import { ButtonComponent } from '../../components/ButtonComponent';
+import { ModalComponent } from '../../components/ModalComponent';
+import { MenuAPI, MenuEndpoint } from '../../api/MenuAPI';
+import { MenuFormScreen } from './screens/MenuFormScreen';
 
 export class MenuPage extends BaseComponent {
     constructor(element: HTMLElement) {
@@ -14,7 +17,7 @@ export class MenuPage extends BaseComponent {
                 <h2>Menu Management</h2>
                 <div id="menuTabsPlaceholder"></div>
                 <div class="d-flex justify-content-end p-2">
-                <div id="openAddMenuItemModalButtonPlaceholder"></div>                    
+                <div id="addMenuButtonPlaceholder"></div>                    
                 </div>
                 <div id="menuTablePlaceholder"></div>                
             </section>
@@ -22,38 +25,91 @@ export class MenuPage extends BaseComponent {
 
         this.element.innerHTML = html;
 
-        this.renderMenuTabs();
+        MenuPage.renderMenuTabs('foodmenu');
 
-        this.renderAddMenuItemModalButton();
+        this.renderAddMenuButton();
 
-        this.renderMenuTable();
+        MenuPage.renderMenuTable('foodmenu');
     }
 
     /**Render Menu Tabs */
-    private renderMenuTabs() {
+    public static renderMenuTabs(activeKey: 'foodmenu' | 'drinkmenu') {
         const menuTabsPlaceholder = <HTMLDivElement>(
             document.getElementById('menuTabsPlaceholder')
         );
-        const menuTabs = new MenuTabs(menuTabsPlaceholder);
+        const menuTabs = new NavComponent(menuTabsPlaceholder, {
+            type: 'tab',
+            activeKey: activeKey,
+            items: [
+                {
+                    key: 'foodmenu',
+                    label: /* html */ `
+                        <span>Food Menu </span>
+                        <i class="bi bi-egg-fried"></i>
+                    `,
+                },
+                {
+                    key: 'drinkmenu',
+                    label: /* html */ `
+                        <span>Drink Menu </span>
+                        <i class="bi bi-cup-straw"></i>
+                    `,
+                },
+            ],
+            /* Tabs change */
+            onChange: (activeKey) => {
+                MenuPage.renderMenuTable(<MenuEndpoint>activeKey.toLowerCase());
+            },
+        });
         menuTabs.render();
     }
 
     /**Render Add Menu Item Modal Button */
-    private renderAddMenuItemModalButton() {
-        const openAddMenuItemModalButtonPlaceholder = <HTMLDivElement>(
-            document.getElementById('openAddMenuItemModalButtonPlaceholder')
+    private renderAddMenuButton() {
+        const addMenuButtonPlaceholder = <HTMLDivElement>(
+            document.getElementById('addMenuButtonPlaceholder')
         );
-        new AddMenuItemModalButton(
-            openAddMenuItemModalButtonPlaceholder
-        ).render();
+
+        const buttonComponent = new ButtonComponent(addMenuButtonPlaceholder, {
+            className: 'btn btn-primary',
+            children: /* html */ `
+                    <span>Add</span>
+                    <i class="bi bi-plus-circle"></i>
+                `,
+            onClick: () => {
+                const modalPlaceholder = <HTMLElement>(
+                    document.getElementById('modalPlaceholder')
+                );
+
+                new ModalComponent(modalPlaceholder, {
+                    setTitle: (element) => {
+                        element.innerHTML = 'Add Menu';
+                    },
+                    setBody: (element) => {
+                        new MenuFormScreen(element, { type: 'add' }).render();
+                    },
+                }).render();
+                ModalComponent.show();
+            },
+        });
+        buttonComponent.render();
     }
 
     /**Render Menu Table */
-    private renderMenuTable() {
-        const menuTablePlaceholder = <HTMLDivElement>(
-            document.getElementById('menuTablePlaceholder')
-        );
-        const menuTable = new MenuTable(menuTablePlaceholder);
-        menuTable.render();
+    public static async renderMenuTable(endpoint: MenuEndpoint) {
+        try {
+            const res = await new MenuAPI(endpoint).getAll();
+            const data = await res.json();
+
+            const menuTablePlaceholder = <HTMLDivElement>(
+                document.getElementById('menuTablePlaceholder')
+            );
+            const menuTableScreen = new MenuTableScreen(menuTablePlaceholder, {
+                data: data,
+            });
+            menuTableScreen.render();
+        } catch (error) {
+            console.log('Render Menu Table Error', error);
+        }
     }
 }
