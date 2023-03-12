5173f30760b9ccb8e78069cf06d016c9c7dc6217
Lee TB <tblyduc2412@gmail.com>
2023-03-12 16:37:14 +0700

    docs: examples for CheckboxGroupComponent

---
+++ src/components/CheckboxGroupComponent/examples.ts
@@ -0,0 +1,15 @@
+export {};
+// const options = [
+//     { label: 'Apple', value: 'Apple' },
+//     { label: 'Pear', value: 'Pear' },
+//     { label: 'Orange', value: 'Orange' },
+// ];
+
+// new CheckboxGroupComponent(<HTMLDivElement>document.getElementById('root'), {
+//     options: options,
+//     className: 'd-flex gap-3',
+//     onChange: (checkedValues: string[]) => {
+//         console.log(checkedValues);
+//     },
+//     defaultValues: ['Pear', 'Orange'],
+// }).render();
