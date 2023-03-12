f7cdd7d4525e8fe45cdb55b6822f9ee81e79d83b
Lee TB <tblyduc2412@gmail.com>
2023-03-13 05:49:52 +0700

    start code bill

---
+++ src/data/bill.ts
@@ -0,0 +1,9 @@
+import { Bill } from '../models/interfaces/Bill';
+
+export const billList: Bill[] = [
+    {
+        id: '1',
+        customer: 'tran bui ly duc',
+        itemList: [{}],
+    },
+];
--- src/pages/BillPage/BillPage.ts
+++ src/pages/BillPage/BillPage.ts
@@ -1,5 +1,6 @@
 import { BaseComponent } from '../../components/BaseComponent';
 import { NavComponent } from '../../components/NavComponent';
+import { CreateBill } from './screens/CreateBill';
 
 export class BillPage extends BaseComponent {
     constructor(element: HTMLElement) {
@@ -9,13 +10,16 @@ export class BillPage extends BaseComponent {
         const html = /*html */ `
             <section id="bill">
                 <h2>Bill Management</h2>
-                <div id="billTabsPlaceholder"></div>
+                <div id="billTabsPlaceholder" class="mb-5"></div>
+                <div id="billBodyPlaceholder"></div>
             </section>
         `;
 
         this.element.innerHTML = html;
 
         this.renderBillTabs();
+
+        this.renderCreateBill();
     }
 
     private renderBillTabs() {
@@ -43,9 +47,23 @@ export class BillPage extends BaseComponent {
             ],
             /* Tabs change */
             onChange: (activeKey) => {
-                console.log(activeKey);
+                if (activeKey === 'CreateNew') {
+                    this.renderCreateBill();
+                } else if (activeKey === 'BillList') {
+                    this.renderBillList();
+                }
             },
         });
         billTabsComponent.render();
     }
+
+    private renderCreateBill() {
+        const billBodyPlaceholder = <HTMLDivElement>(
+            document.getElementById('billBodyPlaceholder')
+        );
+
+        new CreateBill(billBodyPlaceholder).render();
+    }
+
+    private renderBillList() {}
 }
