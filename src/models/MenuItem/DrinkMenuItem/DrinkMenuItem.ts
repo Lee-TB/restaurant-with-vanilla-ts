5a4a6d02a1df4e6f29ede74fac12aa0cd58be4c6
Lee TB <tblyduc2412@gmail.com>
2023-03-13 05:28:53 +0700

    delete: unneccessary files

--- src/models/MenuItem/DrinkMenuItem/DrinkMenuItem.ts
+++
@@ -1,9 +0,0 @@
-import { MenuType } from '../../enums/MenuType';
-import { MenuItem, MenuItemProps } from '../MenuItem';
-
-export class DrinkMenuItem extends MenuItem {
-    private menuType: MenuType = MenuType.DrinkMenu;
-    constructor(props: MenuItemProps) {
-        super(props);
-    }
-}
--- src/models/MenuItem/FoodMenuItem/FoodMenuItem.ts
+++
@@ -1,9 +0,0 @@
-import { MenuType } from '../../enums/MenuType';
-import { MenuItem, MenuItemProps } from '../MenuItem';
-
-export class FoodMenuItem extends MenuItem {
-    private menuType: MenuType = MenuType.FoodMenu;
-    constructor(props: MenuItemProps) {
-        super(props);
-    }
-}
