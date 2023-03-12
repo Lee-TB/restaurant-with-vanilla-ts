814f7975199f93e71c27d82c1de1a9d6806fdb68
Lee TB <tblyduc2412@gmail.com>
2023-03-12 23:34:45 +0700

    Refactor: root variable

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,5 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import './style.css';
 import { App } from './app';
 
-new App(<HTMLDivElement>document.getElementById('root')).render();
+const root = <HTMLDivElement>document.getElementById('root');
+new App(root).render();
